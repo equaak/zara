b0f086913d1f8434d84b2b3f2f52a05e8a90154f
equaak <diasyerm@gmail.com>
2023-01-21 18:48:55 +0600

    fixed after block

--- src/Main.js
+++ src/Main.js
@@ -394,22 +394,22 @@ const Main = () => {
                 <div className='after-part'>
                   Самую полную систему бизнеса байера от А до Я и методику построения продаж на миллионы
                 </div>
+              </div>
+              <div className='after-row'>
                 <div className='after-part'>
                   Развёрнутую базу
                   прямых поставщиков,
                   ссылок и источников
                 </div>
-              </div>
-              <div className='after-row'>
                 <div className='after-part'>
                   Знания и навыки, которые начнёте применять уже во время курса
                 </div>
+              </div>
+              <div className='after-row'>
                 <div className='after-part'>
                   Большое community
                   единомышленников и сеть партнёров
                 </div>
-              </div>
-              <div className='after-row last-row'>
                 <div className='after-part'>
                   Возможности достойного заработка и выхода на новый уровень
                 </div>
